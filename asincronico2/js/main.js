b865cdd9c5a4e9f7b9f7796c6ffa01117dfc8675
Alejandra Ruiz <103595178+AlejandraRuizP@users.noreply.github.com>
2022-05-10 15:25:46 -0400

    Add files via upload

---
+++ asincronico2/js/main.js
@@ -0,0 +1,18 @@
+    function diaActual(){
+    let fecha= new Date();
+    let dia=fecha.getDay();//indice
+    let dias={
+        0: "Domingo",
+        1: "Lunes",
+        2: "Martes",
+        3:"Miercoles",
+        4:"Jueves",
+        5:"Viernes",
+        6:"Sabado"
+    };//evalua valor del indice dentro del rreglo
+    let dActual= dias[dia];
+    return dActual;
+}
+alert("Hoy es " +diaActual());
+
+
